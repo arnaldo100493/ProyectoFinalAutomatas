30f1ca37c43415049abbf083b198f426d7974fcc
Arnaldo Andrés Barrios Mena <proveedor_shernandez@ath.com.co>
2019-11-25 13:57:16 -0500

    Subida de proyecto a repositorio Nuevo Paquete.

---
+++ ProyectoFinalAutomatas/src/java/automatas/Automatas.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package automatas;
+
+/**
+ *
+ * @author abarrime
+ */
+public class Automatas {
+    
+    public Automatas(){
+        
+    }
+    
+}
